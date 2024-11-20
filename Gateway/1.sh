9728593fd77a5658aae023c4ae37f2a9f66a0d83
Rachappa <newrachappabiradar786@gmail.com>
2024-11-20 07:20:25 +0530

    1.sh update to download the node.js

---
+++ Gateway/1.sh
@@ -0,0 +1,14 @@
+sudo apt-get update 
+sudo apt-get upgrade 
+sudo apt-get remove nodejs
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+# download and install Node.js (you may need to restart the terminal)
+nvm install 18
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v18.20.5`
+
+# verifies the right npm version is in the environment
+npm -v # should print `10.8.2`
